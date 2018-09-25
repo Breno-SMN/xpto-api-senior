d61250160c3d69ba188a3868a0fb9ab703720def
Breno Silva <breno.rodrigues@democrata.com.br>
2018-09-25 10:01:20 -0300

    Commit inicial do projeto

---
+++ api-xpto-senior/src/main/java/br/com/xpto/ApiXptoSeniorApplication.java
@@ -0,0 +1,12 @@
+package br.com.xpto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiXptoSeniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiXptoSeniorApplication.class, args);
+	}
+}
---
+++ api-xpto-senior/src/main/java/br/com/xpto/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.com.xpto;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiXptoSeniorApplication.class);
+	}
+
+}
